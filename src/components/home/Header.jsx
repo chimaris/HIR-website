876776a276fea3642fd814d3ca4c712b5262f474
Idy Udoh <idy@idudoh.com>
2023-04-04 11:02:07 +0100

    minor updates

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='w-full h-auto bg-[#3CABDD]'>
         
         <h3 className='bg-[#FFC914] text-[22px]' style={{padding:'20px 20px 10px'}}><span className='mr-4 font-bold'>
-            Latest news in HIR:</span> Launch of the new platform for data reporting</h3>
+            Latest news in EPR:</span> Launch of the new platform for data reporting</h3>
         <div className='flex justify-around mt-20 gap-4 text-white'>
             <div className='w-[30%] md:flex flex-col items-center mt-20 hidden'>
                 <img src={img} alt="boy" className='relative bottom-0'/>
@@ -28,7 +28,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className='relative -bottom-16 bg-white text-black p-16 text-center rounded-md'>
-                    <p className='text-2xl md:text-3xl'>The HIR Datahub is a platform to share links, and exchange subnational programme data, in support of the work HIR is doing to help</p>
+                    <p className='text-2xl md:text-3xl'>The EPR Collaborative hub is a platform to share links, and exchange subnational programme data, in support of the work EPR is doing to help</p>
                 </div>
             </div>
         </div>
